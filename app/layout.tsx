01cf9eb1d8a47a95bb22ec17183bd6ef16bb7fac
dondycles <johnrod.dondoyano@gmail.com>
2024-02-29 10:24:58 +0800

    modified:   app/layout.tsx 	new file:   public/summary.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,37 @@ import QueryProvider from "@/components/QueryProvider";
 import ThemeProvider from "@/components/theme-provider";
 export const metadata: Metadata = {
   title: "incognitalk.",
-  description: "incognitalk.",
+  description: "Reaveling the words anonimously.",
+  metadataBase: new URL("https://incogni-talk.vercel.app/"),
+  twitter: {
+    title: "incognitalk.",
+    description: "Reaveling the words anonimously.",
+    card: "summary_large_image",
+    creator: "@dondycles",
+    images: {
+      url: "/summary.png",
+      alt: "incognitalk.",
+    },
+  },
+  openGraph: {
+    title: "incognitalk.",
+    description: "Reaveling the words anonimously.",
+    type: "website",
+    siteName: "incognitalk.",
+    url: "https://incogni-talk.vercel.app/",
+    images: [
+      {
+        url: "/summary.png",
+        width: 800,
+        height: 800,
+      },
+      {
+        url: "/summary.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
